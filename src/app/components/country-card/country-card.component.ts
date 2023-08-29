8bf9733a6b14691b25eb49f17ba38a8d0575d5ea
Nelson Gutierrez <nelson.gutierrez.vega@gmail.com>
2023-08-29 17:17:14 -0600

    update to latest ng version, minor changes

--- src/app/components/country-card/country-card.component.ts
+++ src/app/components/country-card/country-card.component.ts
@@ -4,10 +4,10 @@ import { CommonModule, NgOptimizedImage } from '@angular/common';
 @Component({
   selector: 'app-country-card',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage],
+  imports: [NgOptimizedImage, CommonModule],
   templateUrl: './country-card.component.html',
   styles: [],
 })
 export class CountryCardComponent {
-  @Input({ required: true }) country: any; // type this
+  @Input({ required: true }) country: any;
 }
